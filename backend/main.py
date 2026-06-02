0f94ea2a253fdde37b18243fa250b22038339517
vincent <mnvictw@163.com>
2026-06-02 16:13:14 +0800

    清楚文件，使用工具生成项目

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, HTTPException, Request, status
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from config.exception import setup_exception_handlers
-
-app = FastAPI()
-
-# 全局异常注册
-setup_exception_handlers(app)
